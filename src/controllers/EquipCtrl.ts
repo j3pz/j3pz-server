3eda94960dd34b91bf135166847e500e07b262e7
ziofat <printlts@gmail.com>
2019-08-02 13:04:31 +0800

    chore: rename declaration as model

--- src/controllers/EquipCtrl.ts
+++ src/controllers/EquipCtrl.ts
@@ -1,6 +1,6 @@
-import { Controller, Get } from '@tsed/common';
-import { Equip } from '../declarations/Equip';
-import { ServerResponse } from '../declarations/Server';
+import { Controller, Get, PathParams } from '@tsed/common';
+import { Equip } from '../model/Equip';
+import { ServerResponse } from '../model/Server';
 
 @Controller('/equip')
 export class EquipCtrl {
@@ -8,6 +8,23 @@ export class EquipCtrl {
     private list(): ServerResponse<Equip[]> {
         return {
             data: [],
+        };
+    }
+
+    @Get('/:id')
+    private detail(@PathParams('id') id: string): ServerResponse<Equip> {
+        return {
+            data: {
+                id: parseInt(id, 10),
+                type: 'equip',
+                attributes: {
+                    category: 'hat',
+                    name: 'test',
+                    tags: [],
+                    quality: 2000,
+                    icon: 5656,
+                },
+            },
         };
     }
 }
