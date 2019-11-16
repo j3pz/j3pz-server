0f9ba96cde94daf8e6a69763f4e98bc5665deb25
ziofat <printlts@gmail.com>
2019-11-16 21:46:36 +0800

    style: disable eslint for register

--- scripts/mocha/register.js
+++ scripts/mocha/register.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires import/no-extraneous-dependencies */
 const Chai = require('chai');
 const ChaiAsPromised = require('chai-as-promised');
 const SinonChai = require('sinon-chai');
