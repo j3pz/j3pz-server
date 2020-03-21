bdf3cbea32149e13ebccb8183160b99b95322775
ziofat <printlts@gmail.com>
2020-03-21 19:44:20 +0800

    test: add url id test

--- src/model/UrlId.ts
+++ src/model/UrlId.ts
@@ -6,15 +6,20 @@ export class UrlId {
 
     public url: string;
 
+    public constructor() {
+        this.objectId = new ObjectID();
+        this.url = encode(Buffer.from(this.objectId.toHexString()));
+    }
+
     public static fromHex(hex: string): UrlId {
-        const urlId = new UrlId();
+        const urlId = Object.create(this.prototype);
         urlId.objectId = ObjectID.createFromHexString(hex);
         urlId.url = encode(Buffer.from(hex));
         return urlId;
     }
 
     public static fromUrl(id: string): UrlId {
-        const urlId = new UrlId();
+        const urlId = Object.create(this.prototype);
         urlId.url = id;
         urlId.objectId = ObjectID.createFromHexString(decode(id).toString());
         return urlId;
