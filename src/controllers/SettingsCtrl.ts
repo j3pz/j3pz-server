1b9bd25931a1e63132f670cc0067697122302053
ziofat <printlts@gmail.com>
2020-10-31 03:46:34 +0800

    feat: add announcment

--- src/controllers/SettingsCtrl.ts
+++ src/controllers/SettingsCtrl.ts
@@ -6,6 +6,12 @@ import {
 } from '@tsed/swagger';
 import { ConfigService } from '../services/ConfigService';
 
+type Announcement = {
+    version: string;
+    title: string;
+    content: string;
+}
+
 @Controller('/settings')
 export class SettingsCtrl {
     public constructor(private configService: ConfigService) {}
@@ -13,8 +19,20 @@ export class SettingsCtrl {
     @Get('/quality-range')
     @Summary('获取装备品级范围')
     @Returns(200, { description: 'OK' })
-    public async detail(): Promise<[number, number, number]> {
+    public async range(): Promise<[number, number, number]> {
         const { minQuality, qualityTick, maxQaulity } = this.configService.getConfig();
         return [minQuality, qualityTick, maxQaulity];
+    }
+
+    @Get('/announcement')
+    @Summary('获取运营公告')
+    @Returns(200, { description: 'OK' })
+    public async announcement(): Promise<Announcement> {
+        const { announcementVersion, announcement, announcementTitle } = this.configService.getConfig();
+        return {
+            version: announcementVersion,
+            title: announcementTitle,
+            content: announcement,
+        };
     }
 }
--- src/model/GlobalConfig.ts
+++ src/model/GlobalConfig.ts
@@ -14,6 +14,7 @@ export interface RawGlobalConfig {
     qualityRange: ConfigObject<string>;
     talent: ConfigObject<boolean>;
     dbVersion: ConfigObject<string>;
+    announcement: ConfigObject<string>;
 }
 
 export const defaultConfig: RawGlobalConfig = {
@@ -29,6 +30,10 @@ export const defaultConfig: RawGlobalConfig = {
         Value: '1.0.0.0',
         SettingType: SettingType.TEXT,
     },
+    announcement: {
+        Value: '||',
+        SettingType: SettingType.TEXT,
+    },
 };
 
 export class GlobalConfig {
@@ -42,6 +47,12 @@ export class GlobalConfig {
 
     public dbVersion: string;
 
+    public announcementVersion: string;
+
+    public announcementTitle: string;
+
+    public announcement: string;
+
     private parseConfig<T>(config: ConfigObject<T>): T {
         return config.Value;
     }
@@ -50,6 +61,7 @@ export class GlobalConfig {
         this.updateQuality(this.parseConfig(rawConfig.qualityRange));
         this.talent = this.parseConfig(rawConfig.talent);
         this.dbVersion = this.parseConfig(rawConfig.dbVersion);
+        [this.announcementVersion, this.announcementTitle, this.announcement] = this.parseConfig(rawConfig.announcement);
     }
 
     private updateQuality(range: string): void {
